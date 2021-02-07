21a678098751ba366d21a48f0919a899766603ba
Morgado <morgadodesarrollador@gmail.com>
2021-02-07 20:50:31 +0100

    pipes

---
+++ Jardineria/appJardineria/src/app/interfaces/ProductosInterface.ts
@@ -0,0 +1,38 @@
+export interface IProducto {
+    CodigoProducto?: number;
+    Nombre?: string;
+    Proveedor?: string;
+    LimiteCredito?: number;
+    Descripcion?: string;
+    CantidadEnStock?: number;
+    PrecioVenta?: number,
+    PrecioProveedor?: number,
+    imagen?: string;
+    Gama?: IGama;
+}
+export interface IGama {
+    Gama: string;
+    Descripcion: string;
+    Imagen: string;
+    productos?: IProducto[]
+}
+
+type GGamas = IGama[] | IGama;
+export interface MsnApiGamas {
+    status?: string;
+    message?: string;
+    errors?: string;
+    code?: number;
+    data?: IGama;
+//    data?: G;
+}
+export interface MsnApiProductos {
+    status?: string;
+    message?: string;
+    errors?: string;
+    code?: number;
+//    data?: IGama[] | IProducto[];
+    data?: IProducto[] | IProducto;
+}
+
+
---
+++ Jardineria/appJardineria/src/app/pipes/pipes.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ViewImagePipe } from './view-image.pipe';
+import { TruncatePipe } from './truncate.pipe';
+
+
+
+@NgModule({
+  declarations: [ViewImagePipe, TruncatePipe],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ViewImagePipe,
+    TruncatePipe
+  ]
+})
+export class PipesModule { }
