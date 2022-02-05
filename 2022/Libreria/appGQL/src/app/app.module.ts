8996cdfef811779a550e620ee7a104797549271f
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-02-05 22:20:59 +0100

    1-.app Inicial Angular APOLLO

--- 2022/Libreria/appGQL/src/app/app.module.ts
+++ 2022/Libreria/appGQL/src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql/graphql.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,6 +18,6 @@ import { GraphQLModule } from './graphql/graphql.module';
   ],
   providers: [],
   bootstrap: [AppComponent],
-  
+
 })
 export class AppModule { }
--- 2022/Libreria/appGQL/src/app/graphql/graphql.module.ts
+++ 2022/Libreria/appGQL/src/app/graphql/graphql.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {APOLLO_OPTIONS} from 'apollo-angular';
 import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
+import { onError } from '@apollo/client/link/error';
 
-
-const uri = ''; // <-- add the URL of the GraphQL server here
+const uri = 'http://localhost:3026/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+
+
   return {
-    link: httpLink.create({uri: 'http://localhost:3026/graphql'}),
+    link: httpLink.create({uri}),
     cache: new InMemoryCache(),
 
   };
