bd33046fe819fc3ee02888193bbc271eb1860cd1
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-02-05 22:24:35 +0100

    2-.gestion de errores GraphQL

--- 2022/Libreria/appGQL/src/app/graphql/graphql.module.ts
+++ 2022/Libreria/appGQL/src/app/graphql/graphql.module.ts
@@ -1,15 +1,23 @@
 import {NgModule} from '@angular/core';
 import {APOLLO_OPTIONS} from 'apollo-angular';
-import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
+import {ApolloClientOptions, ApolloLink, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
 import { onError } from '@apollo/client/link/error';
 
 const uri = 'http://localhost:3026/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
-
+  //capturar errores de GraphQL o de la red
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors){
+        console.log('Error de GraphQL', graphQLErrors)
+      }
+      if (networkError){
+        console.log('Error de Red', networkError)
+      }
+  })
 
   return {
-    link: httpLink.create({uri}),
+    link: ApolloLink.from([errorLink, httpLink.create({uri})]),
     cache: new InMemoryCache(),
 
   };
